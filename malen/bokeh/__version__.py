ad33fba88bc3193048157afe04af231479154787
N. Sanchirico <Lnk2past@gmail.com>
2020-11-29 20:43:58 -0500

    Update __version__.py

--- malen/bokeh/__version__.py
+++ malen/bokeh/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'malen.bokeh'
 __description__ = 'Bokeh extension for malen.'
 __url__ = 'https://github.com/Lnk2past/malen-bokeh'
-__version__ = '2020.10.0'
+__version__ = '2020.11.0'
 __author__ = 'Lnk2past'
 __license__ = 'MIT'
